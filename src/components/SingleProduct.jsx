0f967cd1ab97d085730ccde25e1fa20dda7de652
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-11 15:28:19 +0300

    styling commit before drawio

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -4,11 +4,12 @@ export default function SingleProduct(props) {
     const { id, title, description, price, rating, stock, brand, category, thumbnail } = props.item
     return (
         <li className='productCard'>
-            <img src={thumbnail} alt="" />
+            <div className='productPhotoBlock'>
+                <img src={thumbnail} alt="{title}" className='productPhoto' />
+            </div>
             <div className='productText'>
-                <h2>{title}</h2>
-                <h4>{category}</h4>
-                <p>{description}</p> {/* show on hover */}
+                <h3>{title} <span className='productCategory'>{category}</span></h3>
+                <p className='productDescription'>{description}</p> {/* show on hover */}
                 <div className='productBonusInfo'>
                     <div className='productLeftText'>
                         <p>{price}</p>
